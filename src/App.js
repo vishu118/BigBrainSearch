3b2aa4b7070eaf3b71067c052d8e988594104d6d
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-05-24 14:06:19 +0530

    folder strucuture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 const App = () => {
+  const [darkTheme , setDarkTheme] = useState(false)
   return (
-    <div>App</div>
+   <div className={darkTheme ? "dark" : ""}>
+   <div className='bg-gray-800 text-white' >
+   <Navbar/>
+    <Footer/>
+   </div>
+   
+
+   </div>
+    
   )
 }
 
