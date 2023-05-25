5a7c90fd925a271a3812cd07ad2e3f1e24fe7312
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-05-25 07:57:54 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Routes from "./components/Routes";
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen  ">
-        <Navbar />
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar/>
         <Routes />
         <Footer />
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark: bg-gray-700 border-hgray-200'>
-      <div className="flex justify-between items-center space-x-5 w-screen ">
-        Navbar
-      </div>
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+    <div className="flex justify-between items-center space-x-5 w-screen ">
+      
+        <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+          Goggl 🔎
+        </p>
+      
+      <button type="button"  className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">Click</button>
     </div>
+  </div>
   )
 }
 
